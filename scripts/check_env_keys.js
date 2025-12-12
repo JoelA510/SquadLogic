bf62e0a4d58bc9ec67c3c517d3dd6c4f8e79820b
JoelA510 <joela510@gmail.com>
2025-12-11 17:58:40 -0800

    chore(cleanup): Delete unused scripts and obsolete documentation

--- scripts/check_env_keys.js
+++
@@ -1,8 +0,0 @@
-import fs from 'node:fs';
-
-const envContent = fs.readFileSync('.env', 'utf8');
-const keys = envContent.split('\n')
-    .map(line => line.split('=')[0].trim())
-    .filter(key => key && !key.startsWith('#'));
-
-console.log('Available keys:', keys);
